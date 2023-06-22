967750397867721c547a83ae5049953972209fe0
HP <HP@SULIMANFANANA>
2023-06-22 09:57:27 +0200

    Add project files.

---
+++ trackingg/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using trackingg.Models;
+
+namespace trackingg.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ trackingg/Models/Issue.cs
@@ -0,0 +1,27 @@
+﻿namespace trackingg.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public IssueType IssueType { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum IssueType
+    {
+        Feature,
+        Bug,
+        Documentation
+    }
+}
